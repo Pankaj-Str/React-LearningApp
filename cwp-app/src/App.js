9da9d483e49d8f16912f4596c47a0b0461a438f5
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-04 05:12:21 +0000

    update and create project

---
+++ cwp-app/src/App.js
@@ -0,0 +1,10 @@
+import './css/App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+function App() {
+  return (
+    <h1>Codeswithpankaj</h1>
+    )
+}
+
+export default App;
