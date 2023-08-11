45879b93edc6817b5049175ab30e6c10f696afa3
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-11 05:07:59 +0000

    Add header...

--- cwp-app/src/App.js
+++ cwp-app/src/App.js
@@ -1,9 +1,16 @@
 import './css/App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './Header';
 function App() {
   return (
+    <>
+    
+    <Header></Header>
     <h1>Codeswithpankaj</h1>
+    </>
+    
+
     )
 }
 
---
+++ cwp-app/src/Header.js
@@ -0,0 +1,17 @@
+function Header(){
+ 
+    return(
+        <>
+         
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Bootstrap
+    </a>
+  </div>
+</nav>
+        </>
+    );
+
+}
+export default Header;
